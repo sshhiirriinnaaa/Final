a4ce9b75a145a2cf2fb8b4eb82b0af78ce5f82e6
Irina <irinashirokova.irina@gmail.com>
2023-05-18 01:33:34 +0300

    add LR7

--- LR7/Source.cpp
+++ LR7/Source.cpp
@@ -1,25 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-
 int main() {
-    char s1[100];
-    char s2[4];
-    int count = 0;
-
-    printf("Vvedite stroku\n");
-    gets(s1);
-    printf("Vvedite podstroku\n");
-    gets(s2);
-    char* p = s1;
-
-    do {
-        p = strstr(p, s2);
-        if (p != NULL) {
-            count++;
-            p = &p[strlen(s2)];
-        }
-    } while (p != NULL);
-
-    printf("Chislo vhozhdeniy = %d\n", count);
-    return 0;
+	int a = 3, b = 5,c;
+	c = a + b;
+	printf(" %d", c);
+	return 0;
 }
